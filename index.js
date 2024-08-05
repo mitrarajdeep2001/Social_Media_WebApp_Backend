25f183bd4cf9971594c14e38f704ef91d31f3f16
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2024-08-05 22:40:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use((req, res, next) => {
   // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Origin", "https://sociobook2023.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", "https://master--sociobook2023.netlify.app/");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
